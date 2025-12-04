d3bdafed57b2e73ef08a127261ef5eb017a31658
Andrew Ralon <andrew.ralon@versantmedia.com>
2025-12-04 13:50:56 -0500

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,28 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
+
 
   tags = {
     Name = "HelloWorld"
@@ -32,13 +49,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
-  name = "blog_new"
 
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
+  name   = "blog"
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
